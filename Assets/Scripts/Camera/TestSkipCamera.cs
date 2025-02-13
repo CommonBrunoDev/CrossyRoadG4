e832f0033d0a0bde93036839b93e9ac63b63f74a
Francesco Marconi <francemarc99@gmail.com>
2025-02-13 16:41:03 +0100

    02/13 Camera Movement(Fix)
    -Camera movement (Fix)

--- Assets/Scripts/Camera/TestSkipCamera.cs
+++ Assets/Scripts/Camera/TestSkipCamera.cs
@@ -5,15 +5,17 @@ public class TestSkipCamera : MonoBehaviour
     GameObject CameraController_ref;
     [SerializeField, Range(0f, 10f)] float RepositionTime;
 
-    private void Awake()
+    private void Start()
     {
         CameraController_ref = GameObject.FindGameObjectWithTag("CameraController");
     }
+    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            CameraController_ref.GetComponent<I_CameraReaction>().CameraReaction(transform, RepositionTime);
+            Vector3 ActualPosition = transform.position;
+            CameraController_ref.GetComponent<I_CameraReaction>().CameraReaction(ActualPosition, RepositionTime);
         }
     }
 
